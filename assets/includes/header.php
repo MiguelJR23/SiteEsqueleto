a592c607c5ea01c8b436e0327abe12bfcb0fff25
MiguelJR23 <r0b3rts2332@gmail.com>
2026-04-12 13:42:22 -0300

    header

---
+++ assets/includes/header.php
@@ -0,0 +1,20 @@
+<header class="header">
+  <nav class="navbar">
+    
+    <div class="nav-left">
+      <img src="/assets/img/placeholder.jpg" alt="Logo" class="logo">
+    </div>
+
+    <ul class="nav-center">
+      <li><a href="/index.php">Início</a></li>
+      <li><a href="/pages/sobre.php">Sobre nós</a></li>
+      <li><a href="/pages/contato.php">Contato</a></li>
+    </ul>
+
+    <div class="nav-right">
+      <a href="/pages/login.php" class="btn">Login / Cadastro</a>
+      <button id="darkModeToggle">🌙</button>
+    </div>
+
+  </nav>
+</header>
--- index.php
+++ index.php
@@ -1,1 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <title>Esqueleto</title>
 
+  <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+
+  <?php include 'includes/header.php'; ?>
+
+  <main>
+    <h1>Projeto Esqueleto</h1>
+  </main>
+
+  <?php include 'includes/footer.php'; ?>
+
+</body>
+</html>
